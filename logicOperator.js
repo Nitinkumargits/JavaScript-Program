84121bf3e47a4800519ad0b1320b5c638c22c97f
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2023-09-01 15:39:36 +0530

    learn switch statment

---
+++ logicOperator.js
@@ -0,0 +1,10 @@
+const hasDriveLicense = true;
+const hasGoodVision = true;
+
+const isTried = true;
+
+if (hasDriveLicense && hasGoodVision && !isTried) {
+  console.log("You should Drive .");
+} else {
+  console.log("someone else should Drive ");
+}
---
+++ switch.js
@@ -0,0 +1,19 @@
+const day = "mondey";
+
+switch (day) {
+  case "mondey":
+    console.log("plan to date meeting");
+    break;
+
+  case "tuesdy":
+    console.log("goo out");
+    break;
+
+  case "Wednesday":
+  case "thursday":
+    console.log("write code ");
+    break;
+
+  default:
+    console.log("Not a  vaild day");
+}
