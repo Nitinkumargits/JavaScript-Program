1a238c8056fae350ab118240aa1989e1ecf4a62c
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2023-09-03 13:36:09 +0530

    learn loop

---
+++ loopBackWard.js
@@ -0,0 +1,5 @@
+const jonas = ["friend", "kumar", 2037, 1991, "kumar"];
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+  console.log(i, jonas[i]);
+}
---
+++ loopWithinLoop.js
@@ -0,0 +1,7 @@
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`----- Starting Exercise ${exercise}`);
+
+  for (let rep = 1; rep <= 5; rep++) {
+    console.log(`lefting wrap ${rep}`);
+  }
+}
