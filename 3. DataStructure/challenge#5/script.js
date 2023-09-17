5d456cf0ab03af5fd97a0f8847da91c7ad49f0f6
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2023-09-18 01:28:35 +0530

    revision

--- 3. DataStructure/challenge#5/script.js
+++ 3. DataStructure/challenge#5/script.js
@@ -1,8 +1,8 @@
 "use strict";
 // String Methods Practice
 
-const flights =
-  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+// const flights =
+//   "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
 // 🔴 Delayed Departure from FAO to TXL (11h25)
 //              Arrival from BRU to FAO (11h45)
@@ -11,19 +11,19 @@ const flights =
 
 // console.log(flights.split("+"));
 
-const getcode = (str) => str.slice(0, 3).toUpperCase();
+// const getcode = (str) => str.slice(0, 3).toUpperCase();
 
-for (const flight of flights.split("+")) {
-  //   console.log(flight.split(";"));
-  const [type, from, to, time] = flight.split(";");
+// for (const flight of flights.split("+")) {
+//   //   console.log(flight.split(";"));
+//   const [type, from, to, time] = flight.split(";");
 
-  const output = `${type.startsWith("_Delayed") ? "🔴" : " "}${type.replaceAll(
-    "_",
-    " "
-  )} ${getcode(from)} to ${getcode(to)} (${time.replace(":", "h")})`.padStart(
-    40,
-    " "
-  );
+//   const output = `${type.startsWith("_Delayed") ? "🔴" : " "}${type.replaceAll(
+//     "_",
+//     " "
+//   )} ${getcode(from)} to ${getcode(to)} (${time.replace(":", "h")})`.padStart(
+//     40,
+//     " "
+//   );
 
-  console.log(output);
-}
+//   console.log(output);
+// }
--- 3. DataStructure/tempCodeRunnerFile.js
+++ 3. DataStructure/tempCodeRunnerFile.js
@@ -1,3 +1,43 @@
-ate"));
+const restaurant = {
+  name: "classic Italino",
+  location: "Via Angelo Tavanti 23 , Firenze , Italy",
+  Categories: ["Italino", "Pizzeria", "Vegetarian", "Organic"],
+  StarterMenu: ["Facaccia", "Bruschetta", "Galic-Bread", "caprese-Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+  openingHourse: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, //open 24 hours
+      close: 24,
+    },
+  },
+  order: function (starterIndex, mainMenuIndex) {
+    return [this.StarterMenu[starterIndex], this.mainMenu[mainMenuIndex]];
+  },
+  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+    console.log(
+      `Order received ! ${this.StarterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
 
-// console.log(announcement.replace(/door/g, "nitin"));
+  orderPizza: function (mainIngredient, ...otherIngredient) {
+    console.log(mainIngredient);
+    console.log(otherIngredient);
+  },
+};
+
+// const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+// for (const day of days) {
+//   const open = restaurant.openingHourse[day]?.open ?? "close";
+//   console.log(`On ${day}. we open at ${open}`);
+// }
+
+console.log(restaurant.order?.(2, 0) ?? "method is not exist !!!");
