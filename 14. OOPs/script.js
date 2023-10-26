fcd57f49c9e66bf9b47adeea30d0cfd9127be22d
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2023-10-26 18:59:50 +0530

    learn about constructor and prototype

--- 14. OOPs/script.js
+++ 14. OOPs/script.js
@@ -1,15 +1,91 @@
 'use strict';
 
+// const Person = function (firstName, BirthYear) {
+//   this.firstName = firstName;
+//   this.BirthYear = BirthYear;
+//   //Neve do this...
+//   // this.calcAge = function () {
+//   //   console.log(2037 - this.BirthYear);
+//   // };
+// };
+
+// const Nitin = new Person('nitin', 2000);
+// const ekta = new Person('ekta', 2001);
+// const ved = new Person('Ved', 2003);
+
+// Person.prototype.calcAge = function () {
+//   console.log(2037 - this.BirthYear);
+// };
+
+// Nitin.calcAge();
+// ekta.calcAge();
+// ved.calcAge();
+
+//PROTOTYPE
+
+// const Person = function (firstName, BirthYear) {
+//   this.firstName = firstName;
+//   this.BirthYear = BirthYear;
+// };
+// const jonas = new Person('jonas', 1971);
+
+// Person.prototype.calcAge = function () {
+//   console.log(2037 - this.BirthYear);
+// };
+
+// // jonas.calcAge();
+// // console.log(jonas.__proto__);
+// // console.log(jonas.__proto__.__proto__);
+// // console.log(jonas.__proto__.__proto__.__proto__);
+
+// const arr = [3, 6, 6, 3, 5, 9, 9];
+
+// Array.prototype.unique = function () {
+//   return [...new Set(this)];
+// };
+
+// // console.log(arr.unique());
+
+// const h1 = document.querySelector('h1');
+// console.dir(h1);
+
 const Person = function (firstName, BirthYear) {
   this.firstName = firstName;
   this.BirthYear = BirthYear;
-  //Neve do this...
-  this.calcAge = function () {
-    console.log(2037 - this.BirthYear);
-  };
 };
 
-const Nitin = new Person('nitin', 2000);
-const ekta = new Person('ekta', 2001);
-const ved = new Person('Ved', 2003);
-console.log(Nitin, ekta, ved);
+const jonas = new Person('Jonas', 1971);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.BirthYear);
+};
+jonas.calcAge();
+
+// console.log(Person.prototype);
+// console.log(jonas.__proto__);
+// console.log(jonas.__proto__.__proto__);
+// console.log(jonas.__proto__.__proto__.__proto__);
+// console.dir(Person.prototype.constructor);
+
+// const arr = [3, 6, 6, 5, 6, 9, 9];
+// console.log(arr);
+
+// challenge 1
+
+const Car = function (BrandName, Speed) {
+  this.BrandName = BrandName;
+  this.Speed = Speed;
+};
+
+Car.prototype.accelerate = function () {
+  console.log(this.Speed + 10);
+};
+Car.prototype.break = function () {
+  console.log(this.Speed - 5);
+};
+
+const car1 = new Car('BMW', 120);
+const car2 = new Car('Mercedes', 95);
+
+car1.break();
+car2.accelerate();
