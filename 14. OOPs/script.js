3e0f1719c83e08b7ab9a6ee1b53050b3a8f750b1
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2023-10-26 19:03:39 +0530

    ES6 classes

--- 14. OOPs/script.js
+++ 14. OOPs/script.js
@@ -46,20 +46,20 @@
 
 // // console.log(arr.unique());
 
-// const h1 = document.querySelector('h1');
-// console.dir(h1);
+// // const h1 = document.querySelector('h1');
+// // console.dir(h1);
 
-const Person = function (firstName, BirthYear) {
-  this.firstName = firstName;
-  this.BirthYear = BirthYear;
-};
+// const Person = function (firstName, BirthYear) {
+//   this.firstName = firstName;
+//   this.BirthYear = BirthYear;
+// };
 
-const jonas = new Person('Jonas', 1971);
+// const jonas = new Person('Jonas', 1971);
 
-Person.prototype.calcAge = function () {
-  console.log(2037 - this.BirthYear);
-};
-jonas.calcAge();
+// Person.prototype.calcAge = function () {
+//   console.log(2037 - this.BirthYear);
+// };
+// jonas.calcAge();
 
 // console.log(Person.prototype);
 // console.log(jonas.__proto__);
@@ -72,20 +72,43 @@ jonas.calcAge();
 
 // challenge 1
 
-const Car = function (BrandName, Speed) {
-  this.BrandName = BrandName;
-  this.Speed = Speed;
-};
+// const Car = function (BrandName, Speed) {
+//   this.BrandName = BrandName;
+//   this.Speed = Speed;
+// };
 
-Car.prototype.accelerate = function () {
-  console.log(this.Speed + 10);
-};
-Car.prototype.break = function () {
-  console.log(this.Speed - 5);
-};
+// Car.prototype.accelerate = function () {
+//   console.log(this.Speed + 10);
+// };
+// Car.prototype.break = function () {
+//   console.log(this.Speed - 5);
+// };
 
-const car1 = new Car('BMW', 120);
-const car2 = new Car('Mercedes', 95);
+// const car1 = new Car('BMW', 120);
+// const car2 = new Car('Mercedes', 95);
 
-car1.break();
-car2.accelerate();
+// car1.break();
+// car2.accelerate();
+
+// ES6 CLASSES
+
+class Personcl {
+  constructor(firstName, BirthYear) {
+    this.firstName = firstName;
+    this.BirthYear = BirthYear;
+  }
+
+  calcAge() {
+    console.log(2037 - this.BirthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+}
+
+const nitin = new Personcl('Nitin', 2000);
+
+console.log(nitin);
+nitin.calcAge();
+nitin.greet();
